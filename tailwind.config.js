158897b45bb2472015e3f96689435344363a9cd6
PTheN <dpthen@proton.me>
2026-05-02 20:18:45 -0500

    Rework using Astro + Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class', // o 'media' si prefieres que se active automáticamente
+  theme: {
+    extend: {
+      colors: {
+        // Personaliza tu paleta de colores aquí
+        primary: '#3b82f6', // Azul
+        secondary: '#10b981', // Verde
+      },
+    },
+  },
+};
